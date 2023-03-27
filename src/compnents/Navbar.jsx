c9f23235d8776cce587438efac80c7d670aec0a3
avinashkvar <k3076183@gmail.com>
2023-03-27 10:41:17 +0530

    navbar

--- src/compnents/Navbar.jsx
+++ src/compnents/Navbar.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
 import "./styles/navbar.css"
 import surface from '../static/surface1.svg'
+import landing from '../static/landing.svg'
 const Navbar = () => {
   return (
     <div id='navbar'>
-        <div>
-             <h3>CreativeProxies</h3>
-             <p>Pricing</p>
-             <p>Explore Tools</p>
-             <p>Resources</p>
-             <p>FAQs</p>
-             <p>Blogs</p>
-        </div>
-        <div>
-            <p className='blue'> <img src={surface} alt="" /> Join Discord</p>
-            <p> | Login</p>
-            <button id='started'>Get Started</button>
-        </div>
+        <img src={landing} alt="" />
     </div>
   )
 }
